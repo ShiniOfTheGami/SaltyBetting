fc5f567ffbc2d5311be02f65fe1f1ae07de8b856
ShiniOfTheGami <ctbuschmann@gmail.com>
2015-06-15 21:31:21 +0200

    created empty script

---
+++ script.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name ShiniOfTheGami's automated Tournament mode!
+// @namespace https://github.com/ShiniOfTheGami/SaltyBetting
+// @description A script that bets during saltybet tournaments for you.
+// @version 0.0.1
+// @match *://www.saltybet.com
+// @grant none
+// @updateURL https://raw.githubusercontent.com/ShiniOfTheGami/SaltyBetting/master/script.js
+// @downloadURL https://raw.githubusercontent.com/ShiniOfTheGami/SaltyBetting/master/script.js
+// ==/UserScript==
